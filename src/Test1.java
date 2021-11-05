f2fa118311967f7549cbce2206f6cb9edee75bba
jgoldberg8 <89888020+jgoldberg8@users.noreply.github.com>
2021-11-05 19:37:17 -0400

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -9,25 +9,25 @@ public class Test1 {
     public static double basicCalculator(final Operation operation, final int num1, final int num2){
         double result = 0;
 
-        
-            if (operation.equals("ADD")) {
+        switch(operation) {
+            case ADD:
                 // if operation = ADD, return the sum
                 result = num1 + num2;
-            }
-            if (operation.equals("SUBTRACT")) {
+                break;
+            case SUBTRACT:
                 result = num1 - num2;
-            }
-            if (operation.equals("MULTIPLY")) {
+                break;
+            case MULTIPLY:
                 result = num1 * num2;
-            }
-            if (operation.equals("DIVIDE")) {
+                break;
+            case DIVIDE:
                 if (num2 == 0) {
                     System.out.println("Can't divide by 0");
                 } else {
                     result = ((double)num1) / num2;
                 }
-            }
-               
+                break;
+        }
         return result;
     }
 
